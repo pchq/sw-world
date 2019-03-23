0a0e907f59541f754d798f4fe439715fea666801
pchq <9292358@gmail.com>
2019-03-23 23:21:39 +0800

    Updates

---
+++ precache-manifest.f85eee5e256d9a138705d57d4085fdf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2707b1a1c89523b6c665",
+    "url": "/sw-world/static/js/runtime~main.2707b1a1.js"
+  },
+  {
+    "revision": "2fcae1df16406bc66a2a",
+    "url": "/sw-world/static/js/main.2fcae1df.chunk.js"
+  },
+  {
+    "revision": "79038d9d22d6f7ed038c",
+    "url": "/sw-world/static/js/1.79038d9d.chunk.js"
+  },
+  {
+    "revision": "2fcae1df16406bc66a2a",
+    "url": "/sw-world/static/css/main.932f4da3.chunk.css"
+  },
+  {
+    "revision": "b38a6b775b2eba5cd7e7c28dd4a44eb3",
+    "url": "/sw-world/index.html"
+  }
+];
